694d1d010c7f7a51f9be822968d3d36da576cf9c
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-09 15:46:13 +0300

    Step 1: MongoDb connected and some fixes

--- WebApp.Strategy/Migrations/20220709123238_AddProductEntity.Designer.cs
+++ WebApp.Strategy/Migrations/20220709123238_AddProductEntity.Designer.cs
@@ -4,14 +4,16 @@ using BaseProject.Web.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BaseProject.Web.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220709123238_AddProductEntity")]
+    partial class AddProductEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
