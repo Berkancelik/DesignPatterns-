6da4be6eee6e870fa445cad27dbb8ac083cded8a
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-09 16:06:11 +0300

    step 2: IProductrepository has been created

---
+++ WebApp.Strategy/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetById(string id);
+        Task<List<Product>> GetAllByUserId(string userId);  
+        Task Add(Product product);
+        Task Update(Product product);  
+        Task Delete(Product product);
+    }
+}
