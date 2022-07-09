c1d052fd8f9548bc4498f9b005eebe4194bcec67
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-09 22:33:06 +0300

    Settings and Settings controller has been fixed

---
+++ WebApp.Strategy/Controllers/SettingsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Controllers
+{
+    [Authorize]
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            Settings settings = new();
+            if(User.Claims.Where(x=> x.Type == Settings.claimDatabaseType).FirstOrDefault() != null)
+            {
+                settings.DatabaseType =(EDatabaseType) int.Parse(User.Claims.First(x => x.Type == Settings.claimDatabaseType).Value);
+            }
+            else
+            {
+                settings.DatabaseType = settings.GetDefaultDatabaseType;
+            }
+
+            return View(settings);
+        }
+    }
+}
