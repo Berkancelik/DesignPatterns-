466f245421106699f8b4fdb48d55fa98250f6995
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-09 15:21:55 +0300

    WebApp.Strategy project has been created

---
+++ WebApp.Strategy/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProject.Web.Models
+{
+    public class Context:IdentityDbContext<AppUser>
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+    }
+}
