33251d78bc45d076533150cdc3d48b95221f19a8
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-09 13:25:48 +0300

    Account Controller added

---
+++ BaseProject.Web/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using BaseProject.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BaseProject.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(email);
+            if (hasUser == null) return View();
+            var signInResult = await _signInManager.PasswordSignInAsync(hasUser, password, true, false);
+            if (!signInResult.Succeeded)
+            {
+                return View();
+            }
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+          
+        }
+    }
+}
