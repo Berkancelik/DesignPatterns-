89fd0237e923b8873dc16ccf2ac890af18e0049b
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-10 13:18:16 +0300

    PrimeUserCardTemplate has been completed

---
+++ WebApp.Template/UserCards/PrimeUserCardTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace WebApp.Template.UserCards
+{
+    public class PrimeUserCardTemplate : UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("'<a href='#' class='card-link'> Mesaj Gönder </a>");
+            sb.Append("'<a href='#' class='card-link'> Deteylı Profil </a>");
+            return sb.ToString();
+        }
+
+        protected override string SetPicture()
+        {
+            return $"<img class='card - img - top' src='{AppUser.PictureUrl}'>";
+        }
+    }
+}
