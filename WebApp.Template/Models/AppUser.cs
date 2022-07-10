ae3ee11cb0621df24769acf5e54a0e2b9a47c6b8
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-10 13:05:45 +0300

    User car template has been creaated

--- WebApp.Template/Models/AppUser.cs
+++ WebApp.Template/Models/AppUser.cs
@@ -4,5 +4,8 @@ namespace BaseProject.Web.Models
 {
     public class AppUser:IdentityUser
     {
+        public string PictureUrl { get; set; }
+        public string Description { get; set; }
+
     }
 }
---
+++ WebApp.Template/UserCards/UserCardTemplate.cs
@@ -0,0 +1,38 @@
+﻿using BaseProject.Web.Models;
+using System;
+using System.Text;
+
+namespace WebApp.Template.UserCards
+{
+    public abstract class UserCardTemplate
+    {
+        protected AppUser AppUser { get; set; }
+
+        public void SetUSer(AppUser appUser)
+        {
+            AppUser = appUser;
+        }
+
+        public string Build()
+        {
+            if(AppUser == null) throw new ArgumentNullException(nameof(AppUser));
+
+            var sb = new StringBuilder();
+
+            sb.Append("<div class='card'>");
+            sb.Append(SetPicture());
+            //@ ile birlikte istediğimiz gibi girinti verebiliriz.
+            sb.Append($@"<div class='card-body'>
+                        <h5>{AppUser.UserName}</h5>
+                        <p>{AppUser.Description}</p>");
+            sb.Append(SetFooter());
+            sb.Append("</div>");
+            sb.Append("</div>");
+
+            return sb.ToString();   
+
+        }
+        protected abstract string SetFooter();
+        protected abstract string SetPicture();
+    }
+}
