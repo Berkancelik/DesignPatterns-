1b3687b13ddd1976f2115bb1dc3dcba7b1dce780
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-14 11:21:56 +0300

    Book Composite class has been fixed

---
+++ WebApp.Composite/Composite/BookComposite.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebApp.Composite.Composite
+{
+    public class BookComposite : IComponent
+    {
+        public BookComposite(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private List<IComponent> _components;
+
+        public void Add(IComponent component)
+        {
+            _components.Add(component);
+        }
+
+        public void Remove(IComponent component)
+        {
+            _components.Remove(component);
+        }
+
+
+
+
+
+        public int Count()
+        {
+            return _components.Sum(x => x.Count());
+        }
+
+        public string Display()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"<div class text ='text-primary  my-1'><a href='#' class='menu'>{Name}({Count()})<a/></div>");
+
+            if (!_components.Any()) return sb.ToString();
+            sb.Append("<ul class='list-group list-group-flash'>");
+            foreach (var item in _components)
+            {
+                sb.Append(item.Display());
+            }
+            sb.Append("</ul>");
+            return sb.ToString();
+
+        }
+    }
+}
