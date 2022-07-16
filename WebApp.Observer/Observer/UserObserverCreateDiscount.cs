7ab5b2895597f47383d51d061e93b01c8fe8a0bf
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-16 11:33:50 +0300

    USerObserverCreateDiscount class created

---
+++ WebApp.Observer/Observer/UserObserverCreateDiscount.cs
@@ -0,0 +1,29 @@
+﻿using BaseProject.Web.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WebApp.Observer.Web.Observer
+{
+    public class UserObserverCreateDiscount : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObserverCreateDiscount(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void CreateUSer(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverCreateDiscount>>();
+
+            var context = _serviceProvider.GetRequiredService<Context>();
+            context.Discounts.Add(new Models.Discount { Rate = 10, UserId = appUser.Id });
+            context.SaveChanges();
+            logger.LogInformation("Discount Created")
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
