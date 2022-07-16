5fc8b329fe8b90e174b84e0c50754a0052ef2218
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-16 11:30:16 +0300

    IUserObser interface created

---
+++ WebApp.Observer/Observer/UserObserverWriteToConsole.cs
@@ -0,0 +1,24 @@
+﻿using BaseProject.Web.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WebApp.Observer.Web.Observer
+{
+    public class UserObserverWriteToConsole : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObserverWriteToConsole(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void CreateUSer(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverWriteToConsole>>();
+            logger.LogInformation($"User Created : Id {appUser.Id}");
+            throw new System.NotImplementedException();
+        }
+    }
+}
