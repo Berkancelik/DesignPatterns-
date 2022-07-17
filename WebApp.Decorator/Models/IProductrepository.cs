fc4d498544493c68eb01c64dfc4ee45f14882915
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-17 14:10:24 +0300

    IProductRepository class has been created and CRUD operations has been completed

---
+++ WebApp.Decorator/Models/IProductrepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApp.Decorator.Models
+{
+    public interface IProductrepository
+    {
+        Task<Product> GetById(int id);
+        Task<List<Product>> GetAll();
+        Task<List<Product>> GetAll(string userId);
+        Task<Product> Save(Product product);
+        Task Update(Product product);
+        Task Remove(Product product);
+    }
+}
