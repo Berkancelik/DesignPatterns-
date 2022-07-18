923d1d88f407729ae41bc28a7bc98356580fdf0c
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-18 23:03:23 +0300

    Invoker operations has been created

---
+++ WebApp.Command/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using BaseProject.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Command.Commands;
+using WebApp.Command.Models;
+
+namespace WebApp.Command.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly Context _context;
+public ProductController(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View( await _context.Products.ToListAsync());
+        }
+
+        public async Task<IActionResult> CreateFiles()
+        {
+            var products = await _context.Products.ToListAsync();
+            ExcelFile<Product> excelFile = new(products);
+            PdfFile<Product> pdfFile = new(products, HttpContext);
+            FileCreateInvoker fileCreateInvoker = new();
+
+            fileCreateInvoker.AddCommand(new CreateExcelTableActionCommand<Product>(excelFile));
+            fileCreateInvoker.AddCommand(new CreatePdfTableActionCommand<Product>(pdfFile));
+
+            var filesResult = fileCreateInvoker.CreateFile();
+
+            using (var zipMemoryStream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(zipMemoryStream, ZipArchiveMode.Create))
+                {
+                    foreach (var item in filesResult)
+                    {
+                        var fileContent = item as FileContentResult;
+
+                        var zipFile = archive.CreateEntry(fileContent.FileDownloadName);
+
+                        using (var zipEntryStream = zipFile.Open())
+                        {
+                            await new MemoryStream(fileContent.FileContents).CopyToAsync(zipEntryStream);
+                        }
+                    }
+                }
+
+                return File(zipMemoryStream.ToArray(), "application/zip", "all.zip");
+            }
+        }
+    }
+}
