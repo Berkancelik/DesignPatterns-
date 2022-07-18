b6eeaaa52272f6da8595f2ce5ba4abec20e4971d
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-18 22:25:28 +0300

    FileCreateInvoker clas has been completed

---
+++ WebApp.Command/Commands/FileCreateInvoker.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApp.Command.Commands
+{
+    public class FileCreateInvoker
+    {
+        private  ITableActionCommand _actionCommand;
+        private List<ITableActionCommand> tableActionCommands = new List<ITableActionCommand>();
+
+        public void SetCommand(ITableActionCommand tableActionCommand)
+        {
+            _actionCommand = tableActionCommand;
+        }
+
+        public void AddCommand(ITableActionCommand tableActionCommand)
+        {
+            tableActionCommands.Add(tableActionCommand);
+        }
+
+        public List<IActionResult> CreateFile()
+        {
+             return tableActionCommands.Select(x => x.Execute()).ToList();
+        }
+    }
+}
